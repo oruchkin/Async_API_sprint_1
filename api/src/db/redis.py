bc1526a5a121202799b91e9a311badd64539b175
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-04-06 22:51:05 -0400

    Use configurations

--- api/src/db/redis.py
+++ api/src/db/redis.py
@@ -1,10 +1,21 @@
 from typing import Optional
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from redis.asyncio import Redis
 
 redis: Optional[Redis] = None
 
 
-# Функция понадобится при внедрении зависимостей
+class RedisSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="REDIS_")
+    host: str = "127.0.0.1"
+    port: int = 6379
+
+
 async def get_redis() -> Redis:
+    global redis
+    if redis is None:
+        settings = RedisSettings()
+        redis = Redis(host=settings.host, port=settings.port, decode_responses=True)
+
     return redis
