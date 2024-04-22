cd6d7807c1b97406ec71b7cb8deed170ad87ce36
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-04-22 23:49:18 +0200

    Make tests work

---
+++ api/tests/functional/conftest.py
@@ -0,0 +1,59 @@
+import json
+import os
+from typing import Any
+
+import aiohttp
+import pytest_asyncio
+from elasticsearch import AsyncElasticsearch
+from elasticsearch.helpers import async_bulk
+
+from .settings import ElasticsearchSettings, FastAPISettings
+
+
+@pytest_asyncio.fixture(name="es_client", scope="session")
+async def es_client():
+    es_esttings = ElasticsearchSettings()
+    es_client = AsyncElasticsearch(hosts=es_esttings.url, verify_certs=False)
+    yield es_client
+    await es_client.close()
+
+
+@pytest_asyncio.fixture(name="http_client", scope="session")
+async def http_client():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest_asyncio.fixture(name="es_write_data")
+def es_write_data(es_client):
+    async def inner(data: list[dict]):
+        if await es_client.indices.exists(index="movies"):
+            await es_client.indices.delete(index="movies")
+        await es_client.indices.create(index="movies", body=_get_schema("movies"))
+
+        updated, errors = await async_bulk(client=es_client, actions=data)
+
+        if errors:
+            raise Exception("Ошибка записи данных в Elasticsearch")
+
+    return inner
+
+
+@pytest_asyncio.fixture(name="make_get_request")
+def make_get_request(http_client):
+    api_settings = FastAPISettings()
+
+    async def inner(path: str, query_data: dict):
+        url = api_settings.url + path
+        async with http_client.get(url, params=query_data) as response:
+            body = await response.json()
+            return (response, body)
+
+    return inner
+
+
+def _get_schema(index: str) -> Any:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(f"{dir_path}/testdata/schemas/{index}.json", "r") as fp:
+        return json.load(fp)
---
+++ api/tests/functional/src/test_search.py
@@ -0,0 +1,60 @@
+import datetime
+import uuid
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "query_data, expected_answer",
+    [
+        ({"search": "The Star"}, {"status": 200, "length": 50}),
+        ({"search": "Mashed potato"}, {"status": 200, "length": 0}),
+    ],
+)
+@pytest.mark.asyncio
+async def test_search(make_get_request, es_write_data, query_data, expected_answer):
+
+    # 1. Генерируем данные для ES
+    es_data = [
+        {
+            "id": str(uuid.uuid4()),
+            "imdb_rating": 8.5,
+            "genre": ["Action", "Sci-Fi"],
+            "title": "The Star",
+            "description": "New World",
+            "director": ["Stan"],
+            "actors_names": ["Ann", "Bob"],
+            "writers_names": ["Ben", "Howard"],
+            "actors": [
+                {"id": "ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95", "name": "Ann"},
+                {"id": "fb111f22-121e-44a7-b78f-b19191810fbf", "name": "Bob"},
+            ],
+            "writers": [
+                {"id": "caf76c67-c0fe-477e-8766-3ab3ff2574b5", "name": "Ben"},
+                {"id": "b45bd7bc-2e16-46d5-b125-983d356768c6", "name": "Howard"},
+            ],
+            "created_at": datetime.datetime.now().isoformat(),
+            "updated_at": datetime.datetime.now().isoformat(),
+            "film_work_type": "movie",
+        }
+        for _ in range(60)
+    ]
+
+    bulk_query: list[dict] = []
+    for row in es_data:
+        data = {"_index": "movies", "_id": row["id"]}
+        data.update({"_source": row})
+        bulk_query.append(data)
+
+    # 2. Загружаем данные в ES
+    try:
+        await es_write_data(bulk_query)
+        # 3. Запрашиваем данные из ES по API
+        (response, body) = await make_get_request("/api/v1/search", query_data)
+
+        # 4. Проверяем ответ
+
+        assert response.status == expected_answer.status
+        assert len(body) == expected_answer.length
+    except Exception as err:
+        print(err)
