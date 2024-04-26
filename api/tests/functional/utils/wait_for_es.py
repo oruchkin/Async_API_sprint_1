5b15df797d6777624c5c61b3f7037d3b3bc4b725
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-04-26 19:24:25 +0200

    Fix wait scripts

--- api/tests/functional/utils/wait_for_es.py
+++ api/tests/functional/utils/wait_for_es.py
@@ -5,13 +5,18 @@ from elasticsearch import Elasticsearch
 
 from ..settings import ElasticsearchSettings
 
-logger = logging.Logger(__name__)
+logging.basicConfig()
+logging.root.setLevel(logging.NOTSET)
+logging.basicConfig(level=logging.NOTSET)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     es_settings = ElasticsearchSettings()
     es_client = Elasticsearch(hosts=es_settings.url, validate_cert=False, use_ssl=False)
     while True:
         if es_client.ping():
+            logger.info("Elasticsearch connected")
             break
         logger.info("Failed to connect to elasticsearch, retrying...")
         time.sleep(1)
--- api/tests/functional/utils/wait_for_redis.py
+++ api/tests/functional/utils/wait_for_redis.py
@@ -5,13 +5,20 @@ from redis import Redis
 
 from ..settings import RedisSettings
 
-logger = logging.Logger(__name__)
+logging.basicConfig()
+logging.root.setLevel(logging.NOTSET)
+logging.basicConfig(level=logging.NOTSET)
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     r_settings = RedisSettings()
     client = redis = Redis(host=r_settings.host, port=r_settings.port, decode_responses=True)
     while True:
-        if client.ping():
+        try:
+            resp = client.ping()
+            logger.info("Redis connected")
             break
-        logger.info("Failed to connect to redis, retrying...")
-        time.sleep(1)
+        except Exception as ex:
+            logger.info("Failed to connect to redis, retrying... %s" % ex)
+            time.sleep(1)
