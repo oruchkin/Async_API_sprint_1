e5c5499a3003f980d433337a62193fbfea5505d4
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-04-28 00:26:48 +0200

    Return system health

--- api/src/main.py
+++ api/src/main.py
@@ -1,11 +1,12 @@
 import logging.config
+
 import uvicorn
-from api.v1 import films, genres, persons
+from api.v1 import films, genres, health, persons
+from core.lifecycle import lifespan
 from core.logger import LOGGING
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-from core.lifecycle import lifespan
 
 load_dotenv()
 logging.config.dictConfig(LOGGING)
@@ -20,12 +21,13 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
     log_config=LOGGING,
-    log_level=logging.DEBUG
+    log_level=logging.DEBUG,
 )
 
 app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
 app.include_router(genres.router, prefix="/api/v1/genres", tags=["genres"])
 app.include_router(persons.router, prefix="/api/v1/persons", tags=["persons"])
+app.include_router(health.router, prefix="/api/v1/health", tags=["health"])
 
 if __name__ == "__main__":
     uvicorn.run(
