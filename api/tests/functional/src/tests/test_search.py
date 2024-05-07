93b79cf7a1fdb244eac2b25fa1503cef5bdc9be6
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-05-06 20:58:22 -0400

    Improve tests

--- api/tests/functional/src/tests/test_search.py
+++ api/tests/functional/src/tests/test_search.py
@@ -4,6 +4,31 @@ import uuid
 import pytest
 from redis.asyncio import Redis
 
+from .utils import construct_es_documents
+
+es_films = [
+    {
+        "id": str(uuid.uuid4()),
+        "imdb_rating": random.randint(0, 10),
+        "genres": ["Action", "Sci-Fi"],
+        "title": "The Star" if ix < 15 else "Mashed potato",
+        "description": "New World",
+        "directors_names": ["Stan"],
+        "actors_names": ["Ann", "Bob"],
+        "writers_names": ["Ben", "Howard"],
+        "actors": [
+            {"id": "ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95", "name": "Ann"},
+            {"id": "fb111f22-121e-44a7-b78f-b19191810fbf", "name": "Bob"},
+        ],
+        "writers": [
+            {"id": "caf76c67-c0fe-477e-8766-3ab3ff2574b5", "name": "Ben"},
+            {"id": "b45bd7bc-2e16-46d5-b125-983d356768c6", "name": "Howard"},
+        ],
+        "directors": [{"id": "ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95", "name": "Stan"}],
+    }
+    for ix in range(20)
+]
+
 
 @pytest.mark.parametrize(
     "query_data, expected_answer",
@@ -20,30 +45,7 @@ from redis.asyncio import Redis
 async def test_search(make_get_request, es_write_data, redis_client: Redis, query_data: dict, expected_answer: dict):
 
     # arrange
-    es_data = [
-        {
-            "id": str(uuid.uuid4()),
-            "imdb_rating": random.randint(0, 10),
-            "genres": ["Action", "Sci-Fi"],
-            "title": "The Star" if ix < 15 else "Mashed potato",
-            "description": "New World",
-            "directors_names": ["Stan"],
-            "actors_names": ["Ann", "Bob"],
-            "writers_names": ["Ben", "Howard"],
-            "actors": [
-                {"id": "ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95", "name": "Ann"},
-                {"id": "fb111f22-121e-44a7-b78f-b19191810fbf", "name": "Bob"},
-            ],
-            "writers": [
-                {"id": "caf76c67-c0fe-477e-8766-3ab3ff2574b5", "name": "Ben"},
-                {"id": "b45bd7bc-2e16-46d5-b125-983d356768c6", "name": "Howard"},
-            ],
-            "directors": [{"id": "ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95", "name": "Stan"}],
-        }
-        for ix in range(20)
-    ]
-
-    bulk_query = [{"_index": "movies", "_id": row["id"], "_source": row} for row in es_data]
+    bulk_query = construct_es_documents("movies", es_films)
     await es_write_data(bulk_query)
 
     # act
