93768ef814b75d4c51ecba0e52de2fb9ca814bad
Andrew Grebennikov <av.grebennikov@gmail.com>
2024-05-06 22:13:05 -0400

    Add cache abstraction

--- api/src/db/redis.py
+++ api/src/db/redis.py
@@ -1,13 +1,25 @@
-from core.settings import RedisSettings
 from redis.asyncio import Redis
+from services.cache.none_storage import NoneCache
+from services.cache.redis_storage import RedisCache
+from services.cache.storage import ICache
 
 redis: Redis | None = None
 
 
 def get_redis() -> Redis:
     global redis
-    if redis is None:
-        settings = RedisSettings()
-        redis = Redis(host=settings.host, port=settings.port, decode_responses=True)
+    if not redis:
+        raise ValueError("Redis is not setup")
 
     return redis
+
+
+def get_cache() -> ICache:
+    # In future we can move it to different location
+    # but as we don't have any other implementations
+    # of the cache provider let's keep it here.
+    global redis
+    if redis is None:
+        return NoneCache()
+
+    return RedisCache(redis)
