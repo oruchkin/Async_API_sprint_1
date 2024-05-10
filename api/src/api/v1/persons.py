c0960535625764ea8cc93756b53b19eaa8aa6913
Oleg <oruchkin@bk.ru>
2024-05-10 23:27:38 +0300

    openAPI documentation

--- api/src/api/v1/persons.py
+++ api/src/api/v1/persons.py
@@ -20,7 +20,10 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-@router.get("/search", response_model=list[Person], summary="Поиск по персонам")
+@router.get("/search",
+            response_model=list[Person],
+            summary="Поиск по персонам",
+            description="Возвращает список персон по поисковому запросу")
 async def search_persons(
     response: Response,
     query: str = Query(..., min_length=3, description="Search string"),
@@ -43,7 +46,10 @@ async def search_persons(
     return persons
 
 
-@router.get("/{person_id}", response_model=Person, summary="Данные по персоне")
+@router.get("/{person_id}",
+            response_model=Person,
+            summary="Данные по персоне",
+            description="Возвращает подробную информацию о персоне")
 async def get_person(
     person_id: UUID, person_film_service: PersonFilmService = Depends(get_person_film_service)
 ) -> Person:
@@ -54,7 +60,10 @@ async def get_person(
     raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="person not found")
 
 
-@router.get("/{person_id}/films", response_model=list[Film], summary="Фильмы по персоне")
+@router.get("/{person_id}/films",
+            response_model=list[Film],
+            summary="Фильмы по персоне",
+            description="Возвращает список фильмов, в которых участвовала персона")
 async def list_person_films(
     response: Response,
     person_id: UUID,
